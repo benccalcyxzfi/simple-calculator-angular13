688e7c2c91b517fbbda003ab4326737e841845f9
benccalcyxzfi <88748637+benccalcyxzfi@users.noreply.github.com>
2022-08-21 19:22:40 -0300

    feat: implements calculator service

--- src/app/calculator/services/calculator.service.ts
+++ src/app/calculator/services/calculator.service.ts
@@ -4,6 +4,31 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CalculatorService {
-
+  //defines the constants used to identify calculation operations
+  static readonly ADDITION: string = '+';
+  static readonly SUBTRACTION: string = '-';
+  static readonly DIVISION: string = '/';
+  static readonly MULTIPLICATION: string = '*';
   constructor() { }
+  calculate(num1: number, num2: number, operation: string): number {
+    let result: number;//stores the result of the operation
+    switch (operation) {
+      case CalculatorService.ADDITION:
+        result = num1 + num2;
+        break;
+      case CalculatorService.SUBTRACTION:
+        result = num1 - num2;
+        break;
+      case CalculatorService.DIVISION:
+        result = num1 / num2;
+        break;
+      case CalculatorService.MULTIPLICATION:
+        result = num1 * num2;
+        break;
+      default:
+        result = 0;
+        break;
+    }
+    return result;
+  }
 }
