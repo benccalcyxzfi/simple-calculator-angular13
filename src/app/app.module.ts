8fe8807981b2999f42f8c04f7c6237fc843c6414
benccalcyxzfi <88748637+benccalcyxzfi@users.noreply.github.com>
2022-08-21 18:01:43 -0300

    feat: create the calculator module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CalculatorModule } from './calculator';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CalculatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
