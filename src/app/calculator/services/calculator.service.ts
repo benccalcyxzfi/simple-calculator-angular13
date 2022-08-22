69a882849577baac57f1b37516c64ecb5737ed6c
benccalcyxzfi <88748637+benccalcyxzfi@users.noreply.github.com>
2022-08-21 21:01:46 -0300

    documenting the calculator service.

--- src/app/calculator/services/calculator.service.ts
+++ src/app/calculator/services/calculator.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -10,6 +11,14 @@ export class CalculatorService {
   static readonly DIVISION: string = '/';
   static readonly MULTIPLICATION: string = '*';
   constructor() { }
+  /**
+   * method that calculates a mathematical operation given two numbers.
+   * Supports addition, subtraction, multiplication and division operations.
+   * @param num1 number
+   * @param num2 number
+   * @param operation string operation to be performed
+   * @returns number Results of operation
+   */
   calculate(num1: number, num2: number, operation: string): number {
     let result: number;//stores the result of the operation
     switch (operation) {
